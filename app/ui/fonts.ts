7822e3419bda06169e2c5d04fc56550b4f91de4c
Umagdiaz <umabaligranado@gmail.com>
2024-02-28 01:17:59 -0300

    pathname and style modification of a component

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,7 @@
 import {Montserrat} from 'next/font/google'
 
 export const montserrat = Montserrat({ subsets: ['latin'] })
+
+import {Lusitana} from 'next/font/google'
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: "400" })
